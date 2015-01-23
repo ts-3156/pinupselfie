98115dbb2362352ddb1e70e795ee881c2400ebc3
Shinohara Teruki <ts_3156@yahoo.co.jp>
2015-01-24 06:22:56 +0900

    write cute.json

--- bin/update_photos.rb
+++ bin/update_photos.rb
@@ -1,18 +1,28 @@
 require 'ex_twitter'
 
 class Script
+  DEBUG = true
+
   CONFIG = YAML.load_file('config/twitter.yml')
 
-  NAMES = %w(0612kazuha AgusuHerudi chantichanti22 juicyloveberry maroka1029
-    mizuki_momoko momo_ninomiya ryoshihono s5hjvwyk shizaki_hinata suzukisakiika
-    tomoe_0116 y_amamiya_y yoshida_saki yukakuramoti)
+  NAMES =
+    if DEBUG
+      %w(0612kazuha)
+    else
+      %w(0612kazuha AgusuHerudi chantichanti22 juicyloveberry maroka1029
+        mizuki_momoko momo_ninomiya ryoshihono s5hjvwyk shizaki_hinata suzukisakiika
+        tomoe_0116 y_amamiya_y yoshida_saki yukakuramoti)
+    end
 
   def run
     users = client.users(NAMES)
     photos = users.map { |user| fetch_photos(user) }.flatten
 
     puts photos.size
-    puts photos.map{|p| p.attrs[:media_url] }
+    puts photos.map { |p| p.attrs[:media_url] }
+
+    json = photos.map { |p| {url: p.attrs[:media_url], link: 'http://example.com/'} }
+    open('assets/json/cute.json', 'w') { |f| f.write(JSON.pretty_generate(json)) }
   end
 
   private
@@ -22,7 +32,7 @@ class Script
   end
 
   def fetch_photos(user)
-    p = client.user_photos(user, {max_paginates: 1, count: 3})
+    p = client.user_photos(user, {max_paginates: 1, count: 30})
     puts "#{user.screen_name}, #{p.size}"
     p
   rescue => e
@@ -32,4 +42,6 @@ class Script
   end
 end
 
-Script.new.run
+if $0 == __FILE__
+  Script.new.run
+end
